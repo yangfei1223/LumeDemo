e72fab4d4fd6b706108e2db3b0af75534eebbe87
Fei Yang <yangfei92516@163.com>
2026-04-08 17:32:22 +0800

    Add input callback interface to IApplication
    Add OnMouseMove, OnMouseButton, OnMouseScroll, OnKey virtual methods
    
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- include/application_interface.h
+++ include/application_interface.h
@@ -15,6 +15,12 @@ public:
     virtual void OnStart() = 0;
     virtual void OnStop() = 0;
     virtual void OnFrame() = 0;
+
+    // Input callbacks
+    virtual void OnMouseMove(double x, double y) = 0;
+    virtual void OnMouseButton(int button, int action, int mods) = 0;
+    virtual void OnMouseScroll(double xoffset, double yoffset) = 0;
+    virtual void OnKey(int key, int scancode, int action, int mods) = 0;
 };
 
 #endif // APPLICATION_INTERFACE_H
